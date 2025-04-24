709a1ee0156585c601cb29b5f205be6bc01c4f9d
paulinacinque <pcinque1@mail.austral.edu.ar>
2025-04-24 12:01:06 -0300

    Update line.py

--- line.py
+++ line.py
@@ -8,7 +8,7 @@ def line():
     print(f"El coeficiente A de su ecuación de la recta es: {A}")
     print(f"El coeficiente B de su ecuación de la recta es: {B}")
     print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
-    print(f"\tEl coeficiente X2 de su ecuación de la recta es: {X2}\n")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}\n")
 
     print("Para la siguiente ecuación:")
     print(f"\tY = {A}X + {B}\n")
